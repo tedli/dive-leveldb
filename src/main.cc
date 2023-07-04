50fc1cbbf02295c9d755020683f5d9b16a526d9f
lizhen <lizhen@outlook.jp>
2023-07-04 16:37:23 +0800

    vendor

--- src/main.cc
+++ src/main.cc
@@ -1,9 +1,14 @@
 #include "include/build_version.h"
 
+#include "leveldb/db.h"
 #include <iostream>
-
 
 int main(int argc, char** argv) {
   std::cout << "Hello, World!, " << build_compile_date << std::endl;
+  leveldb::DB* db;
+  leveldb::Options options;
+  options.create_if_missing = true;
+  leveldb::Status status = leveldb::DB::Open(options, "debug/db", &db);
+  delete db;
   return 0;
 }
